fd6f87447b6faa92009016b0c89c544acf0ac05e
Rainer Hirk <rainer.hirk@quintik.at>
2026-04-23 07:14:46 +0000

    fix class attributes order

--- inst/tinytest/test_as_trackframe_sf_behaviour.R
+++ inst/tinytest/test_as_trackframe_sf_behaviour.R
@@ -1,6 +1,15 @@
-sft_df <- sftrack_mini
-sft_df <- sft_df[, !colnames(sft_df) %in% c("easting", "northing")]
+library(trackframe)
+library(tinytest)
 
+# source(system.file("tinytest/test_helpers.R", package = "trackframe"))
+data("sftrack_mini")
+
+sft_df <- sftrack_mini
+# sft_df <- sft_df[, !colnames(sft_df) %in% c("easting", "northing")]
+sft_df$easting <- NULL
+sft_df$northing <- NULL
+
+data("move2_mini")
 m2_df <- move2_mini
 
 new_x <- seq_len(nrow(sft_df))
@@ -8,9 +17,10 @@ new_y <- seq_len(nrow(sft_df)) + 1
 
 sft_df$x2 <- new_x
 sft_df$y2 <- new_y
+class(sft_df) <- c("sftrack", "sf", "data.frame")
 m2_df$x2 <- new_x
 m2_df$y2 <- new_y
-
+class(m2_df) <- c("move2", "sf", "data.frame")
 tf_from_m2 <- as.trackframe(m2_df, easting_col = "x2", northing_col = "y2")
 tf_from_sft <- as.trackframe(sft_df, easting_col = "x2", northing_col = "y2")
 
@@ -65,9 +75,11 @@ expect_equal(
 expect_warning(as.trackframe(data = m2_df, crs = "4267"))
 
 m2_df$time2 <- m2_df$time
+class(m2_df) <- c("move2", "sf", "data.frame")
 tf_from_m2_2 <- as.trackframe(data = m2_df, time_col = "time2")
 expect_equal(time_col(tf_from_m2_2), "time2")
 
 m2_df$id2 <- "track2"
+class(m2_df) <- c("move2", "sf", "data.frame")
 tf_from_m2_3 <- as.trackframe(data = m2_df, id_col = "id2")
 expect_equal(id_col(tf_from_m2_3), "id2")
