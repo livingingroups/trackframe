0824a04ac326d44c5754b5cd2e0a3cf7288482c2
Rainer Hirk <rainer.hirk@quintik.at>
2026-04-24 12:18:01 +0000

    fix trackframe subsetting

--- R/subset.R
+++ R/subset.R
@@ -5,31 +5,58 @@
 
 #' @noRd
 #' @export
-"[.trackframe" <- function(x, i, j, drop = TRUE, ...) {
-  has_j <- !missing(j)
-  if (missing(i)) {
-    i <- seq_len(NROW(x))
-  }
-  if (missing(j)) {
-    j <- seq_len(NCOL(x))
-  }
-  to_vec <- FALSE
-  if (has_j) {
-    if (length(j) == 1 && drop == TRUE) {
-      to_vec <- TRUE
+"[.trackframe" <- function(x, i, j, drop = NULL, silent = FALSE, ...) {
+  x_attr <- attributes(x)
+  tf_cn <- tf_colnames(x)
+  attr_names <- names(x_attr)
+  x_attr[names(x_attr) %in% c("names", "row.names", "class")] <- NULL
+  class2 <- attr(x, "class")[2]
+  if (is.null(drop)) {
+    if (class2 == "data.frame") {
+      drop <- TRUE
+    } else if (class2 == "data.table") {
+      drop <- FALSE
+    } else if (class2 %in% c("tbl_df", "tbl")) {
+      drop <- FALSE
     }
   }
-  if (length(i) == 1 && length(j) > 1) {
-    drop <- FALSE
-  }
-  if (isTRUE(to_vec)) {
-    obj <- base::`[.data.frame`(x, i, j, drop = drop)
+  if (class2 == "data.table") {
+    mc <- match.call(expand.dots = TRUE)
+    mc[[1L]] <- quote(data.table:::`[.data.table`)
+    mc$drop <- drop
+    mc$silent <- NULL
+    obj <- eval(mc, parent.frame())
   } else {
-    x_attr <- attributes(x)
-    attr_names <- names(x_attr)
-    x_attr[names(x_attr) %in% c("names", "row.names", "class")] <- NULL
+    has_j <- !missing(j)
+    if (missing(i)) {
+      i <- seq_len(NROW(x))
+    }
+    if (missing(j)) {
+      j <- seq_len(NCOL(x))
+    }
+    to_vec <- FALSE
+    if (has_j) {
+      if (length(j) == 1 && drop == TRUE) {
+        to_vec <- TRUE
+      }
+    }
+    if (length(i) == 1 && length(j) > 1) {
+      drop <- FALSE
+    }
     obj <- base::`[.data.frame`(x, i, j, drop = drop)
-    attributes(obj) <- c(attributes(obj), x_attr)[attr_names]
+  }
+  if(!is.null(dim(obj))) {
+    if (all(tf_cn %in% colnames(obj))) {
+      attributes(obj) <- c(attributes(obj), x_attr)[attr_names]
+    } else {
+      if (silent == FALSE) {
+        class(obj) <- class(obj)[!class(obj) %in% "trackframe"]
+        attributes(obj)[c("time", "easting", "northing", "id", "crs", "crs_type",
+          "transformation_info")] <- NULL
+        warning(sprintf("Key col %s removed. Dropping class trackframe.\n",
+          tf_cn[!tf_cn %in% colnames(obj)])) 
+      }
+    }
   }
   return(obj)
 }
--- inst/tinytest/test_subsetting_trackframe.R
+++ inst/tinytest/test_subsetting_trackframe.R
@@ -43,7 +43,11 @@ tf2$id2 <- "id2"
 expect_inherits(tf2, "trackframe")
 expect_inherits(tf2, "data.table")
 
-tf3 <- tf2[, tf_colnames(tf2)]
+
+dt2 <- as.data.table(tf2)
+expect_equal(tf2[, tf_colnames(tf2), with = TRUE], dt2[, tf_colnames(tf2)])
+expect_equal(tf2[, tf_colnames(tf2), with = FALSE], dt2[, tf_colnames(tf2), with = FALSE], check.attributes = FALSE)
+tf3 <- tf2[, tf_colnames(tf2), with = FALSE]
 expect_inherits(tf3, "trackframe")
 expect_inherits(tf3, "data.table")
 attr_tf2 <- attributes(tf2)
@@ -86,3 +90,53 @@ attr_tf2 <- attributes(tf2)
 attr_tf2$row.names <- as.numeric(rownames(tf4))
 expect_equal(attributes(tf4), attr_tf2)
 expect_equal(rownames(tf4), "1")
+
+
+
+###
+# non trackframe vanilla, data.table, tibble
+data(df_mini, package = "trackframe")
+dt_mini <- as.data.table(df_mini)
+tbl_mini <- as_tibble(df_mini)
+
+# trackframe vanilla, data.table, tibble
+df_tf_mini <- trackframe::as.trackframe(df_mini, crs = NA)
+dt_tf_mini <- trackframe::as.trackframe(dt_mini, coerce_to = NULL, crs = NA)
+tbl_tf_mini <- trackframe::as.trackframe(tbl_mini, coerce_to = NULL, crs = NA)
+
+df_tf_mini_time <- df_tf_mini[, "time"]
+expect_true(!any(class(df_tf_mini_time) %in% "trackframe"))
+expect_equal(df_tf_mini_time, df_mini[, "time"])
+
+dt_tf_mini_time <- dt_tf_mini[, "time"]
+expect_warning(dt_tf_mini[, "time"])
+expect_true(!any(class(dt_tf_mini_time) %in% "trackframe"))
+expect_equal(dt_tf_mini_time, dt_mini[, "time"])
+
+tbl_tf_mini_time <- tbl_tf_mini[, "time"]
+expect_warning(tbl_tf_mini[, "time"])
+expect_true(!any(class(tbl_tf_mini_time) %in% "trackframe"))
+expect_equal(tbl_tf_mini_time, tbl_mini[, "time"])
+
+
+dt_tf_mini_time2 <- dt_tf_mini[, time]
+expect_true(!any(class(dt_tf_mini_time2) %in% "trackframe"))
+expect_equal(dt_tf_mini_time2, dt_mini[, time])
+expect_equal(dt_tf_mini[, time, drop = FALSE], dt_mini[, time, drop = FALSE])
+
+df_tf_mini$A <- "A"
+df_tf_mini2 <- df_tf_mini[, c("time", "id", "easting", "northing")]
+expect_equal(class(df_tf_mini2), c("trackframe", "data.frame"))
+
+dt_tf_mini$A <- "A"
+dt_tf_mini2 <- dt_tf_mini[, c("time", "id", "easting", "northing")]
+expect_equal(class(dt_tf_mini2), c("trackframe", "data.table", "data.frame"))
+
+tbl_tf_mini$A <- "A"
+tbl_tf_mini2 <- tbl_tf_mini[, c("time", "id", "easting", "northing")]
+expect_equal(class(tbl_tf_mini2), c("trackframe", "tbl_df", "tbl", "data.frame"))
+
+expect_equal(df_tf_mini[1, "time"], df_mini[1, "time"])
+expect_equal(dt_tf_mini[1, "time"], dt_mini[1, "time"])
+expect_equal(tbl_tf_mini[1, "time"], tbl_mini[1, "time"])
+expect_equal(dt_tf_mini[1, time], dt_mini[1, time])
