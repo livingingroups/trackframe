d7d11c7c29bb4763652c32f9ca4ccdb2c05252a4
Rainer Hirk <rainer.hirk@quintik.at>
2026-05-04 11:26:49 +0000

    fix subsetting

--- R/subset.R
+++ R/subset.R
@@ -27,34 +27,27 @@
     mc$silent <- NULL
     obj <- eval(mc, parent.frame())
   } else {
-    has_j <- !missing(j)
     if (missing(i)) {
       i <- seq_len(NROW(x))
     }
     if (missing(j)) {
       j <- seq_len(NCOL(x))
     }
-    to_vec <- FALSE
-    if (has_j) {
-      if (length(j) == 1 && drop == TRUE) {
-        to_vec <- TRUE
-      }
-    }
     if (length(i) == 1 && length(j) > 1) {
       drop <- FALSE
     }
     obj <- base::`[.data.frame`(x, i, j, drop = drop)
   }
-  if(!is.null(dim(obj))) {
+  if (!is.null(dim(obj))) {
     if (all(tf_cn %in% colnames(obj))) {
       attributes(obj) <- c(attributes(obj), x_attr)[attr_names]
     } else {
       if (silent == FALSE) {
         class(obj) <- class(obj)[!class(obj) %in% "trackframe"]
         attributes(obj)[c("time", "easting", "northing", "id", "crs", "crs_type",
-          "transformation_info")] <- NULL
+            "transformation_info")] <- NULL
         warning(sprintf("Key col %s removed. Dropping class trackframe.\n",
-          tf_cn[!tf_cn %in% colnames(obj)])) 
+            tf_cn[!tf_cn %in% colnames(obj)]))
       }
     }
   }
